89eed3762119132d18fd15ccb5e765f6942d599e
nitisakc <nc.north@gmail.com>
2019-04-08 13:23:11 +0700

    Update RPI3BuildOpenCV.sh

--- RPI3BuildOpenCV.sh
+++ RPI3BuildOpenCV.sh
@@ -25,3 +25,30 @@ fi
 
 echo "Installing dependencies..."
 sudo apt-get update
+sudo apt-get install -y build-essential \ 
+	cmake pkg-config \
+	libjpeg-dev \
+	libtiff5-dev \
+	libjasper-dev \
+	libpng12-dev
+	libavcodec-dev \
+	libavformat-dev \
+	libswscale-dev \
+	libv4l-dev
+	libxvidcore-dev \
+	libx264-dev
+	libgtk2.0-dev \
+	libgtk-3-dev
+	libatlas-base-dev \
+	gfortran
+	
+	
+echo "Installing Python 2..."
+sudo apt-get install -y python-dev python-spidev python-pip
+
+echo "Installing Python 3..."
+sudo apt-get install -y python3-dev python3-spidev python3-pip
+
+echo "Installing numpy scipy"
+sudo pip install numpy scipy
+sudo pip3 install numpy scipy
